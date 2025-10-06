7f7380d5e6037551cd0e16088e165a25d67bafad
krishna-123-star <prabhadevi2505@gmail.com>
2025-10-06 13:15:12 +0530

    Not Found

--- angular-routing-steps/src/app/app.routes.ts
+++ angular-routing-steps/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { About } from './about/about';
+import { NotFound } from './not-found/not-found';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // default route
   { path: 'home', component: Home },
-  { path: 'about', component: About }
+  { path: 'about', component: About },
+  { path: '**', component: NotFound }
 ];
